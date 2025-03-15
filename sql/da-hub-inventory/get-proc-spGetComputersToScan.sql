6b3760c50c95679fb080e6932cf3251aa9f17a0a
Yojana <yojana.hawaii@gmail.com>
2025-03-14 17:57:49 -1000

    stored proc to change scan frequency

--- sql/da-hub-inventory/get-proc-spGetComputersToScan.sql
+++ sql/da-hub-inventory/get-proc-spGetComputersToScan.sql
@@ -5,19 +5,21 @@ drop proc if exists dbo.spGetComputersToScan;
 go
 
 create proc dbo.spGetComputersToScan(
-	@count varchar(3) = 500
+	@count varchar(3) = 500,
+	@scanAfterDays varchar(2) = 6
 )
 as 
 begin
 	declare @cnt int = convert(int, @count);
 	declare @date date = convert(date,getdate() );
+	declare @scanAfter int = convert(int, @scanAfterDays );
 
 	select top (@cnt) ad.ComputerName
 	from DaHubInventory.dbo.AdComputers ad
 		left join DaHubInventory.dbo.WorkstationSpecs ws on ad.ComputerName = ws.ComputerName
 	where 
 		ad.Enabled = 1
-		and (datediff(day,ws.ScanSuccessDate,@date) >= 6 
+		and (datediff(day,ws.ScanSuccessDate,@date) >= @scanAfter
 				or ws.ScanSuccessDate is null 
 				/*or something is wrong > mssing patch or missing bit locker or sentinel one etc*/
 			)
@@ -25,4 +27,4 @@ end
 
 go
 
-exec dbo.spGetComputersToScan @count = 10;
+exec dbo.spGetComputersToScan @scanAfterDays = 1;
