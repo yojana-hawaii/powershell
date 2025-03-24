7450641da93f727a833e7d0f41a887039a021705
Yojana <yojana.hawaii@gmail.com>
2025-03-24 12:20:15 -1000

    Update criteria to search of computer to scan next

---
+++ sql/da-hub-inventory/create-view-spGetComputer.sql
@@ -0,0 +1,104 @@
+use DaHubInventory
+go
+
+drop view if exists dbo.vwWorkstationScanOrder
+go
+create view dbo.vwWorkstationScanOrder
+as
+
+	with u as (
+		select 
+			ad.ComputerName,
+			ad.IPV4Address,
+			ad.HasBitlocker, ad.Haslaps, 
+			ad.OperatingSystem, 
+			
+			case when ws.scanattemptdate is null then null else datediff(hour, ws.scanattemptdate, getdate()) end LastScanAttemptHours,
+			case when ws.ScanSuccessDate is null then null else datediff(day,  ws.ScanSuccessDate ,getdate()) end LastSuccessfulScanDays,
+			case when ws.ScanSuccessDate is null then 1 else 0 end IsNeverScanned,
+			case when ad.LastLogonDate is null then null else datediff(day,  ad.LastLogonDate ,getdate()) end LastLogonDays,
+			case when ws.LastSecurityUpdateDate is null then null else datediff(day,  ws.LastSecurityUpdateDate ,getdate()) end LastSecurityPatchDays,
+			case when ws.LastSecurityUpdateDate is null then null else datediff(day,  ws.LastSecurityUpdateDate ,getdate()) end LastPatchDays,
+			
+
+			ws.IsServer, ws.IsDesktop, ws.IsLaptop, ws.IsThinClient, ws.IsVm, ws.IsVpn, ws.Offline,
+			case when sen.ServiceStatus = 'Running' then 1 when sen.ServiceStatus is null then null else 0 end  SentinelOneService,
+			case when kac.ServiceStatus = 'Running' then 1 when kac.ServiceStatus is null then null else 0 end  KaceService,
+			case when aid.ServiceStatus = 'Running' then 1 when aid.ServiceStatus is null then null else 0 end  SysaidService,
+			case when denc.ServiceStatus = 'Running' then 1 when denc.ServiceStatus is null then null else 0 end  DellEncryptionService,
+			case when cyl.ServiceStatus = 'Running' then 1 when cyl.ServiceStatus is null then null else 0 end  CylanceService,
+			case 
+				when ws.IsVm = 1 or ws.IsServer = 1 then null  
+				when ws.DiskType = 'SDD' then 1 
+				when ws.DiskType like '%SDD%' then 1
+				when ws.DiskType is null then null 
+				when ws.DiskType = '' then null 
+			else 0 end IsSdd,
+			ws.TpmEnabled,
+			ws.TpmVersion,
+
+			--ws.DiskType,
+			--cyl.ServiceStatus Cylance,
+			--denc.ServiceStatus DellEncryption,
+			--aid.ServiceStatus Sysaid,
+			--kac.ServiceStatus Kace,
+			--sen.ServiceStatus SentinelOne,
+			--ws.LastPatchDate, ws.LastSecurityUpdateDate, 
+			ws.ScanSuccessDate, ws.ScanAttemptDate
+
+			
+
+		from DaHubInventory.dbo.AdComputers ad
+			left join DaHubInventory.dbo.WorkstationSpecs ws on ad.ComputerName = ws.ComputerName 
+			left join DaHubInventory.dbo.WorkstationServices sen on ad.ComputerName = sen.ComputerName and sen.ServiceName = 'SentinelAgent'
+			left join DaHubInventory.dbo.WorkstationServices kac on ad.ComputerName = kac.ComputerName and kac.ServiceName = 'konea'
+			left join DaHubInventory.dbo.WorkstationServices aid on ad.ComputerName = aid.ComputerName and aid.ServiceName = 'SysAidAgent'
+			left join DaHubInventory.dbo.WorkstationServices denc on ad.ComputerName = denc.ComputerName and denc.ServiceName = 'CMGShield'
+			left join DaHubInventory.dbo.WorkstationServices cyl on ad.ComputerName = cyl.ComputerName and cyl.ServiceName like '%cylan%'
+		where 
+			ad.Enabled = 1 
+			and ad.OperatingSystem is not null --2 nas & a nimble
+			and ad.OperatingSystem != 'unknown' -- 5 vCenter & vSphere
+			and ad.ComputerName not in ('kphc-srvfeder01')
+	)
+		select 
+		case
+			when isnull(LastSuccessfulScanDays,6) > 3 
+					and isnull(LastScanAttemptHours,6) > 3 
+					and IPV4Address is not null 
+					and IPV4Address not like '192%' 
+					and OperatingSystem not in ('Windows Server 2003','Windows Server 2012 R2 Standard')
+			then 100
+			else 0
+		end
+		+
+		case 
+			when LastSuccessfulScanDays is null then 4
+			when LastSuccessfulScanDays <=3 then LastSuccessfulScanDays
+			else LastSuccessfulScanDays 
+		end 
+		+ 
+		case
+			when LastScanAttemptHours is null then 4
+			when LastSuccessfulScanDays <= 3 then 0
+			when LastScanAttemptHours <= 3 then -5
+		else LastScanAttemptHours/10 end
+		+ 
+		case 
+			when IPV4Address like '192%' then -10 -- dmz not accessible
+			when OperatingSystem in ('Windows Server 2003','Windows Server 2012 R2 Standard') then -8  -- should be off most of the time
+			when IPV4Address is null then -7 -- possibility of connection
+			when IPV4Address like '10.10.%' then 1 -- VPN focus
+		else 0 end
+		+ IsNeverScanned
+		--isnull(lastlogondays,4) 
+		--+ isnull(LastScanAttemptHours/5,3) 
+		NextScanOrder,
+			* 
+		from u
+
+go
+select * from DaHubInventory.dbo.vwWorkstationScanOrder
+--where NextScanOrder >=3
+order by NextScanOrder desc, LastSuccessfulScanDays
+go
--- sql/da-hub-inventory/get-proc-spGetComputersToScan.sql
+++ sql/da-hub-inventory/get-proc-spGetComputersToScan.sql
@@ -6,47 +6,24 @@ go
 
 create proc dbo.spGetComputersToScan(
 	@count varchar(3) = 500,
-	@scanAfterDays varchar(2) = 6
+	@scanAfterDays varchar(2) = 3
 )
 as 
 begin
 	declare @cnt int = convert(int, @count);
-	declare @date date = convert(date,getdate() );
+	declare @datetime datetime = getdate();
+	declare @date date = convert(date,@datetime );
 	declare @scanAfter int = convert(int, @scanAfterDays );
 	
 	select top (@cnt) 
-		ad.ComputerName, 
-		ws.ScanAttemptDate, ws.ScanSuccessDate, ws.LastPatchDate, ws.LastSecurityUpdateDate, 
-		ad.HasBitlocker, ad.Haslaps, 
-		sen.ServiceStatus SentinelOne,
-		k.ServiceStatus Kace,
-		aid.ServiceStatus Sysaid,
-		dell.ServiceStatus DellEncryption,
-		cyl.ServiceStatus Cylance
-	from DaHubInventory.dbo.AdComputers ad
-		left join DaHubInventory.dbo.WorkstationSpecs ws on ad.ComputerName = ws.ComputerName 
-		left join DaHubInventory.dbo.WorkstationServices sen on ad.ComputerName = sen.ComputerName and sen.ServiceName = 'SentinelAgent'
-		left join DaHubInventory.dbo.WorkstationServices k on ad.ComputerName = k.ComputerName and k.ServiceName = 'konea'
-		left join DaHubInventory.dbo.WorkstationServices aid on ad.ComputerName = aid.ComputerName and aid.ServiceName = 'SysAidAgent'
-		left join DaHubInventory.dbo.WorkstationServices dell on ad.ComputerName = dell.ComputerName and dell.ServiceName = 'CMGShield'
-		left join DaHubInventory.dbo.WorkstationServices cyl on ad.ComputerName = cyl.ComputerName and cyl.ServiceName like '%cylan%'
+		* 
+	from dbo.vwWorkstationScanOrder vw
 	where 
-		ad.Enabled = 1
-		and (datediff(day,ws.ScanSuccessDate,@date) >= @scanAfter 
-				or ws.ScanSuccessDate is null 
-				or (ad.HasBitlocker = 0 and ws.IsServer = 0 and ws.IsThinClient = 0 and ws.IsVm = 0) --bitlocker not in server, thin client or vm
-				or (ad.Haslaps = 0 and ws.IsThinClient = 0 ) -- laps not in thin client yet
-				or DATEDIFF(day, ws.LastPatchDate, @date) >= 45
-				or DATEDIFF(day, ws.LastSecurityUpdateDate, @date) >= 45
-				or isnull(sen.ServiceStatus,'') != 'Running'
-				or isnull(k.ServiceStatus,'') != 'Running'
-				or isnull(aid.ServiceStatus,'') != 'Running'
-				or isnull(dell.ServiceStatus,'') != 'Running'
-				or cyl.ServiceStatus is not null
-			)
-		and datediff(hour, ws.scanattemptdate, @date) >= 3 -- scan failure wait for 3 hours
+		(vw.ScanSuccessDate is null or datediff(day,vw.ScanSuccessDate,@date) >= @scanAfter )
+		and (vw.scanattemptdate is null or datediff(hour, vw.scanattemptdate, @date) >= 3) -- scan failure wait for 3 hours
+	order by NextScanOrder desc
 end
 
 go
 
-exec dbo.spGetComputersToScan @scanAfterDays = 3;
+exec dbo.spGetComputersToScan @scanAfterDays = 2;
