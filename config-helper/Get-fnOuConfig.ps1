f438b4bcc6a46c87df0a25b50a0fbe4428898801
Yojana <yojana.hawaii@gmail.com>
2025-07-09 12:58:06 -1000

    disable users after 14 days inactivity and notify managers

---
+++ config-helper/Get-fnOuConfig.ps1
@@ -0,0 +1,16 @@
+
+function Get-fnOuConfig {
+
+    $global = Get-Content "$PWD\config\ou.conf"
+    
+    $conf = @()
+    
+    $global | ForEach-Object {
+        $keys = $_ -split "="
+        $value = $keys[1] -replace ":", "="
+        $conf += @{$keys[0]=$value}
+    }
+    return $conf
+}
+
+
