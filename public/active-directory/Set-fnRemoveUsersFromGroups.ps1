d0178c3152b7762878a3637deca5293f462f2005
Yojana <yojana.hawaii@gmail.com>
2025-07-10 13:38:01 -1000

    remove all AD users from AD group

---
+++ public/active-directory/Set-fnRemoveUsersFromGroups.ps1
@@ -0,0 +1,52 @@
+set-location "\\fileserver\it\apps\powershell"
+
+function Set-fnRemoveUsersFromGroups {
+    #region - Import necessary configs and private functions #>
+    $configHelper       = @(Get-ChildItem -Path "$PWD\config-helper\Get-fnConfig.ps1"               -ErrorAction SilentlyContinue -Recurse)
+    $emailConfig        = @(Get-ChildItem -Path "$PWD\config-helper\Get-fnEmailConfig.ps1"               -ErrorAction SilentlyContinue -Recurse)
+    $utility            = @(Get-ChildItem -Path "$PWD\private\utility\*.ps1"                        -ErrorAction SilentlyContinue -Recurse)
+    Write-Information "Read public, private & shared functions, stored procedures and config helpers"
+    #import all function
+    foreach ($import in @($configHelper + $utility + $emailConfig)){
+        try{
+            . $import.Fullname
+            Write-Information "importing $($import.Fullname)"
+        } catch {
+            Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
+            $true
+        }  
+    }
+
+    #endregion
+
+
+    $startTimer = Start-Timer
+    Write-Verbose "$($MyInvocation.MyCommand.Name): start." 
+
+    $config = Get-fnConfig 
+    $groupsToEmpty = (($config.groupsToEmpty) -replace '"', "") -split ","
+
+    foreach($group in $groupsToEmpty){
+        Write-Verbose "Checking group: $group"
+        $members = Get-ADGroupMember -Identity $group
+        
+        foreach ($member in $members){
+            write-host "Remove $member from $group"
+            Remove-ADGroupMember -Identity $group -Members $member -Confirm:$False
+        }
+    }
+
+    $totalTime = Stop-Timer -Start $startTimer
+    Write-Information "$($MyInvocation.MyCommand.Name): Disable inactive users complete. It took $totalTime" 
+
+}
+
+$Global:today = $null
+$today = Get-Date
+$filenameAppend = Get-Date -Format "yyyMMddHHmm"
+
+Start-Transcript -Path "$pwd\log\$($MyInvocation.MyCommand.Name)_$filenameAppend.txt" -Append
+$verbosePreference = "continue"
+Set-fnRemoveUsersFromGroups -Verbose -InformationAction continue
+Stop-Transcript
+
