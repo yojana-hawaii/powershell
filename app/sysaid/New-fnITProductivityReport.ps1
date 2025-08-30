b00f28538791663877f5f258cc32e5df4fb6e45d
Yojana <yojana.hawaii@gmail.com>
2025-08-29 20:00:35 -1000

    refactor sysaid

--- app/sysaid/New-fnITProductivityReport.ps1
+++ app/sysaid/New-fnITProductivityReport.ps1
@@ -68,22 +68,16 @@ function fnLocal_SetEmailBody{
     return $email
 } 
 function New-fnITProductivityReport {
-    [CmdletBinding()]
-    param (
-        
-    )
-
     #region - Import necessary configs and private functions #>
 
     Write-Verbose "$($MyInvocation.MyCommand.Name): Import necessary private functions & config helpers in "
-    $configHelper       = @(Get-ChildItem -Path "$PWD\config-helper\Get-fnConfig.ps1" -ErrorAction SilentlyContinue -Recurse)
-    $emailConfig       = @(Get-ChildItem -Path "$PWD\config-helper\Get-fnEmailConfig.ps1" -ErrorAction SilentlyContinue -Recurse)
-    $private            = @(Get-ChildItem -Path "$PWD\private\sysaid\*.ps1"      -ErrorAction SilentlyContinue -Recurse)
-    $utility            = @(Get-ChildItem -Path "$PWD\private\utility\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
-    $storedProcedure    = @(Get-ChildItem -Path "$PWD\stored-procedure\sysaid\*.ps1"      -ErrorAction SilentlyContinue -Recurse)
-    $sqlConection       = @(Get-ChildItem -Path "$PWD\stored-procedure\SqlConnection\*.ps1"      -ErrorAction SilentlyContinue -Recurse)
-
-    foreach ($import in @($configHelper + $emailConfig + $utility + $sqlConection + $storedProcedure + $private)){
+    $private    = @(Get-ChildItem -Path "$PWD\app\sysaid\private\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
+    $utility    = @(Get-ChildItem -Path "$PWD\shared\utility\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
+    $sqlConn    = @(Get-ChildItem -Path "$PWD\shared\SqlConnection\*.ps1"      -ErrorAction SilentlyContinue -Recurse)
+    $config     = @(Get-ChildItem -Path "$PWD\shared\config-helper\Get-fnConfig.ps1"    -ErrorAction SilentlyContinue )
+    $emailConf  = @(Get-ChildItem -Path "$PWD\shared\config-helper\Get-fnEmailConfig.ps1" -ErrorAction SilentlyContinue -Recurse)
+    
+    foreach ($import in @($utility + $private + $sqlConn + $config + $emailConf)){
         try{
             . $import.Fullname
             Write-Information "$($MyInvocation.MyCommand.Name): Importing $($import.Fullname)"
@@ -127,12 +121,10 @@ function New-fnITProductivityReport {
     $totalTime = Stop-Timer -Start $startTimer
     Write-Verbose "$($MyInvocation.MyCommand.Name): Workstation Details complete. It took $totalTime" 
 }
- 
-
-$Global:today = $null
-$today = Get-Date
+$Global:today = Get-Date
 $filenameAppend = Get-Date -Format "yyyMMddHHmm"
 
-Start-Transcript -Path "$pwd\log\$($MyInvocation.MyCommand.Name)_$filenameAppend.txt" -Append
-New-fnITProductivityReport -InformationAction Continue -verbose
+Start-Transcript -Path "$pwd\shared-ignore\log\$($MyInvocation.MyCommand.Name)_$filenameAppend.txt" -Append
+$verbosePreference = "continue"
+New-fnITProductivityReport -Verbose -InformationAction continue
 Stop-Transcript
