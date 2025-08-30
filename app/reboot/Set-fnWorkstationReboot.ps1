1df5874c9d1a063195b6adb47545a38298b5a8d2
Yojana <yojana.hawaii@gmail.com>
2025-08-29 19:53:14 -1000

    refactor reboot script

---
+++ app/reboot/Set-fnWorkstationReboot.ps1
@@ -0,0 +1,79 @@
+set-location "\\fileserver\it\apps\powershell"
+function Set-fnWorkstationReboot {
+    #region - Import necessary configs and private functions #>
+
+    Write-Verbose "$($MyInvocation.MyCommand.Name): Import necessary private functions & config helpers in "
+    $private    = @(Get-ChildItem -Path "$PWD\app\reboot\private\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
+    $utility    = @(Get-ChildItem -Path "$PWD\shared\utility\*.ps1"    -ErrorAction SilentlyContinue -Recurse)
+    $sqlConn    = @(Get-ChildItem -Path "$PWD\shared\SqlConnection\*.ps1"      -ErrorAction SilentlyContinue -Recurse)
+    $config     = @(Get-ChildItem -Path "$PWD\shared\config-helper\Get-fnConfig.ps1"    -ErrorAction SilentlyContinue )
+    
+    foreach ($import in @($utility + $private + $sqlConn + $config)){
+        try{
+            . $import.Fullname
+            Write-Information "$($MyInvocation.MyCommand.Name): Importing $($import.Fullname)"
+        } catch {
+            Write-Error -Message "$($MyInvocation.MyCommand.Name): Failed to import functions from $($import.Fullname): $_"
+            $true
+        }
+        
+    }
+    $import = $null
+    #endregion
+        
+        $startTimer = Start-Timer
+        Write-Verbose "$($MyInvocation.MyCommand.Name): start."
+
+        $weekday = ($today).DayOfWeek
+        
+        $frequency =  if($weekday -eq "sunday")  {'weekly'} else {'daily'}
+        $initiater = "kphc-powershell"
+        
+        Write-Information "$weekday reboot $frequency"
+        
+        # $frequency = "weekly"
+        $computers = Invoke-fnSpWorkstationReboot -frequency $frequency
+        $total = $computers.count
+        $cnt = 1
+
+
+        foreach($computer in $computers){
+            $ping = Test-Connection -ComputerName $computer.ComputerName -BufferSize 4 -count 1 -Quiet
+            write-verbose "Working on $($computer.ComputerName) ... $cnt of $total"
+            if($ping){
+                Write-Information  "Online & rebooting $($computer.ComputerName)"
+
+                try{
+                    if($computer.ComputerName -eq $initiater){
+                        Write-Verbose "Skip $initiator for now."
+                        continue
+                    }
+                    Restart-Computer -ComputerName $computer.ComputerName -force
+                } catch {
+                    write-verbose "try-catch fail $($computer.ComputerName)"
+                }
+
+            } else {
+                write-verbose "cannot reboot $($computer.ComputerName). It is offline."
+            }
+            $cnt++
+        }
+
+        $totalTime = Stop-Timer -Start $startTimer
+        Write-Verbose "$($MyInvocation.MyCommand.Name): Workstation Details complete. It took $totalTime"     
+        
+        if($frequency -eq "weekly"){
+            write-verbose "finally restart $initiater"
+            Restart-Computer -ComputerName $initiater -force
+
+        }
+                        
+
+}
+$Global:today = Get-Date
+$filenameAppend = Get-Date -Format "yyyMMddHHmm"
+
+Start-Transcript -Path "$pwd\shared-ignore\log\$($MyInvocation.MyCommand.Name)_$filenameAppend.txt" -Append
+$verbosePreference = "continue"
+Set-fnWorkstationReboot -Verbose -InformationAction continue
+Stop-Transcript
