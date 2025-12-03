3aef12db4951b222b99ec963a8f67d16a881f8a5
Yojana <yojana.hawaii@gmail.com>
2025-12-03 13:48:40 -1000

    check all DC and all logon date and pick the newest

--- app/users/inactive-users/Get-fnInactiveUsersByOU.ps1
+++ app/users/inactive-users/Get-fnInactiveUsersByOU.ps1
@@ -16,12 +16,21 @@ function Get-fnInactiveUsersByOU {
     * Last Login Before: $lastLoginDateToKeepActive
     * Last Modified Before: $lastModifiedDateToKeep
     * New account created but Never Logged in: $neverLoggedInDateToKeepActive"
+
+    #region - round one filtering
+    # Three filters to get started
+    # 1. user has to be enabled
+    # 2. do not disable accounts modified within past 2 days - something was modified for a reason
+    # 3. logondate from one DC - 14 days
     $inactiveUsers = Get-ADUser -Filter * -Properties * -SearchBase $ou | 
     Where-Object {
         $_.enabled -and # look at only active accounts
         $_.modified -le $lastModifiedDateToKeep -and # if account modifed in last 2 days -> do not disable
-        $_.LastLogonDate -le  $lastLoginDateToKeepActive  #last-logon 14 login
-    } | Select-Object Name, sAMAccountName, Enabled, LastLogonDate, Created, Modified, Description , EmailAddress, Company, @{
+        # $_.LastLogonDate -le  $lastLoginDateToKeepActive  #last-logon 14 login
+        [DateTime]::FromFileTime($_.LastLogon) -le  $lastLoginDateToKeepActive  #last-logon 14 login
+    }  | 
+    Select-Object  Name, sAMAccountName, Enabled, Created, Modified, Description , EmailAddress, Company, 
+    @{
         label = "Manager"
         expression = {
             if($null -ne $_.Manager){ (Get-ADUser -Identity $_.Manager -Properties EmailAddress).emailAddress} else {""}
@@ -29,16 +38,65 @@ function Get-fnInactiveUsersByOU {
     },@{
         label = "Type"
         expression = {if($_.EmailAddress) {$_.EmailAddress} elseif ($_.Company) {$_.Company} else  {$type} }
+    }, LastLogonDate,@{
+        label="LastLogon"
+        expression={ [DateTime]::FromFileTime($_.LastLogon) }
+    },@{
+        label="LastLogonTimeStamp"
+        expression={ [DateTime]::FromFileTime($_.LastLogonTimeStamp) }
     }
+    #endregion
     
-    $inactiveUsers = $inactiveUsers | 
+    #region - Never logged in 
+    # new account created with past 30 days but not started yet.
+    if($inactiveUsers){
+        $inactiveUsers = $inactiveUsers | 
         Where-Object {
             # never login but created within 30 days (new hire account creation)
-            -not ((  $null -eq $_.LastLogonDate -or $_.LastLogonDate -eq "" ) -and 
+            -not ((  $null -eq $_.LastLogon -or $_.LastLogon -eq "" ) -and 
                 $_.Created -ge $neverLoggedInDateToKeepActive)
-            } |
-        Select-Object Name, sAMAccountName, Enabled, LastLogonDate, Created, Modified, Description , EmailAddress,Manager,Type
+        }
+    }
+    #endregion
 
+    #region - check all DC
+    # From the inactive list check, check each domain controller to see if they are logged in 
+    # Last logon does not sync right away (takes 10-15 days) between domain controllers
+    # okta only login does not seem to be syncing back to AD - vendors possiblly premature disable
+
+
+    $dcs = (Get-ADDomainController -filter * ).Name
+
+    foreach($user in $inactiveUsers){
+        $maxDate = [datetime]"1900-01-01"
+        $dcs | ForEach-Object {
+            $dcLogon = Get-ADUser -Identity $user.sAMAccountName -Properties * | 
+            Select-Object sAMAccountName, LastLogonDate,@{
+                    label="LastLogon"
+                    expression={ [DateTime]::FromFileTime($_.LastLogon) }
+                },@{
+                    label="LastLogonTimeStamp"
+                    expression={ [DateTime]::FromFileTime($_.LastLogonTimeStamp) }
+                }
+
+            if($maxDate -lt $dcLogon.LastLogon){
+                $maxDate = $dcLogon.LastLogon
+            }
+            if($maxDate -lt $dcLogon.LastLogonTimeStamp){
+                $maxDate = $dcLogon.LastLogonTimeStamp
+            }
+            if($maxDate -lt $dcLogon.LastLogonDate){
+                $maxDate = $dcLogon.LastLogonDate
+            }
+        }
+        if($maxDate -gt $lastLoginDateToKeepActive){
+            # remove from inactive list
+            $inactiveUsers = $inactiveUsers | Where-Object { $_ -ne $user}
+            continue
+        }
+    }
+
+    #endregion
 
     return $inactiveUsers
 }
