5176d42865149f27dac07730360789d564943c7f
Yojana <yojana.hawaii@gmail.com>
2026-08-17 11:50:43 -1000

    update date type for table value type

--- sql/da-hub-inventory/inventory-type-dbo.tvpWorkstationPartitions.sql
+++ sql/da-hub-inventory/inventory-type-dbo.tvpWorkstationPartitions.sql
@@ -2,20 +2,20 @@
 use DaHubInventory
 go
 
---drop type if exists dbo.tvpWorkstationPartitions;
+drop type if exists dbo.tvpWorkstationPartitions;
 go
 create type dbo.tvpWorkstationPartitions as table (
 	ComputerName varchar(50),
-	PartitionNumber int,
-    DiskNumber int,
-    IsBoot bit,
-    IsHidden bit,
-    IsSystem bit,
-    IsReadOnly bit,
-    IsOffline bit,
-    IsActive bit,
-    DriveLetter varchar(2),
-    PartitionSizeGb float
+	PartitionNumber varchar(50),
+    DiskNumber varchar(50),
+    IsBoot varchar(50),
+    IsHidden varchar(50),
+    IsSystem varchar(50),
+    IsReadOnly varchar(50),
+    IsOffline varchar(50),
+    IsActive varchar(50),
+    DriveLetter varchar(50),
+    PartitionSizeGb varchar(50)
 );
 
 go
