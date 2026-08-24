f918cbbfacb8a9a7993822c1b4e96b1032d75e5d
Yojana <yojana.hawaii@gmail.com>
2026-08-23 16:27:20 -1000

    fix typo

--- app/computer-inventory/Get-fnComputerInventory.ps1
+++ app/computer-inventory/Get-fnComputerInventory.ps1
@@ -41,7 +41,7 @@ function Get-fnComputerInventory {
         $cnt++
     }
 
-    $computers = GetWorkstationsToScan -count 50 -scanAfterHours 24
+    $computers = Invoke-fnSpGetWorkstationsToScan -count 50 -scanAfterHours 24
     $total = $computers.count
     $cnt = 1
     foreach($comp in $computers){
