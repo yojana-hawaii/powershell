0a0f2b7a1b269c121273a156bc85390634fd4015
Yojana <yojana.hawaii@gmail.com>
2026-08-24 16:04:11 -1000

    clean up

--- app/computer-inventory/Get-fnComputerInventory.ps1
+++ app/computer-inventory/Get-fnComputerInventory.ps1
@@ -41,7 +41,7 @@ function Get-fnComputerInventory {
         $cnt++
     }
 
-    $computers = Invoke-fnSpGetWorkstationsToScan -count 50 -scanAfterHours 24
+    $computers = Invoke-fnSpGetWorkstationsToScan -count 100 -scanAfterHours 24
     $total = $computers.count
     $cnt = 1
     foreach($comp in $computers){
--- sql/da-hub-inventory/inventory-proc-spGetWorkstationsToScan.sql
+++ sql/da-hub-inventory/inventory-proc-spGetWorkstationsToScan.sql
@@ -24,8 +24,6 @@ begin
 		isnull(vw.LastSuccessfulScanHours,100) > @scanAfter
 		and isnull(vw.LastScanAttemptHours,100) > @attemptAfter
 		and IsThinClient != 1
-		--(vw.ScanSuccessDate is null or datediff(day,vw.ScanSuccessDate,@date) >= @scanAfter )
-		--and (vw.scanattemptdate is null or datediff(hour, vw.scanattemptdate, @date) >= @attempAfter ) -- scan failure wait for 3 hours
 	order by NextScanOrder desc
 end
 
